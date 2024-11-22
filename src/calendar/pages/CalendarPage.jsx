0872cc95f5dc48c6fafb939e7b70dead24ee887f
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-11-21 22:53:12 -0300

    First commit

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,49 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+import { Navbar } from "../";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Cumpleaños Manu",
+    notes: "Comprar pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Manuel",
+    },
+  },
+];
+
+export const CalendarPage = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
+    </>
+  );
+};
