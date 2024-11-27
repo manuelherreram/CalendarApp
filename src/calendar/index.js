be4919bc0977be64bbd8b3ac34c3bf849bab490a
Manuel Herrera <manuel.herrera.m@gmail.com>
2024-11-27 10:23:46 -0300

    Adds react-modal component

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,3 +1,4 @@
 export * from "./components/Navbar";
+export * from "./components/CalendarModal";
 export * from "./components/CalendarEvent";
 export * from "./pages/CalendarPage";
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -2,7 +2,7 @@ import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 import { addHours } from "date-fns";
-import { CalendarEvent, Navbar } from "../";
+import { CalendarEvent, CalendarModal, Navbar } from "../";
 import { localizer, getMessagesES } from "../../helpers";
 import { useState } from "react";
 
@@ -67,6 +67,7 @@ export const CalendarPage = () => {
         onSelectEvent={onSelect}
         onView={onViewChanged}
       />
+      <CalendarModal />
     </>
   );
 };
