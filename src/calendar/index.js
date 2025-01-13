abe5370b61f58a576f8d21847b8a677d635e926e
Manuel Herrera <manuel.herrera.m@gmail.com>
2025-01-13 11:10:58 -0300

    Preparar la creación de un nuevo evento

--- src/calendar/index.js
+++ src/calendar/index.js
@@ -1,4 +1,5 @@
 export * from "./components/Navbar";
 export * from "./components/CalendarModal";
 export * from "./components/CalendarEvent";
+export * from "./components/FabAddNew";
 export * from "./pages/CalendarPage";
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,7 @@
 import { Calendar } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
-import { CalendarEvent, CalendarModal, Navbar } from "../";
+import { CalendarEvent, CalendarModal, FabAddNew, Navbar } from "../";
 import { localizer, getMessagesES } from "../../helpers";
 import { useState } from "react";
 import { useUiStore, useCalendarStore } from "../../hooks";
@@ -57,6 +57,7 @@ export const CalendarPage = () => {
         onView={onViewChanged}
       />
       <CalendarModal />
+      <FabAddNew />
     </>
   );
 };
